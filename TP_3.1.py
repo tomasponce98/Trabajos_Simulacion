ae02471491b2ee5ff7202fe91565697cdd603707
Tomás Ponce <poncetomaseduardo@gmail.com>
2020-07-01 22:48:43 -0300

    10 Curvas de Clientes en sistema echa, faltan las otras 4 graficas

--- TP_3.1.py
+++ TP_3.1.py
@@ -260,62 +260,71 @@ def inicializar():
     time_arrival=[]
 
 
-
-
-#Ingresa media, media de servicio, tiempo de finalizacion
-Q_limit=100
-num_events=2
 mean_interarrival = float( input("media del arribo: "))
 mean_service=float(input("media del servicio: "))
 num_delays_required = float(input("num delays requiered: "))
 
-numero_clientes_cola=input("Cuantos clientes en cola habra?")
-
-
-#Inicializa
-
-inicializar()
-
-
-
-#continua 
-#while(num_custs_delayed<num_delays_required):
-#arribos=0
-#departos=0
-
-while(num_custs_delayed < num_delays_required):
-    timing()
-    update_time_avg_state()
-
-    if(next_event_type==1):
-        arrive()
-        #arribos+=1
-
-        
-    elif(next_event_type==2):
-        depart()
-        #departos+=1
-report()
-
-
-
-
-def GraficasDemoras():
-    global total_of_delays
+def MainProgram():
+#Ingresa media, media de servicio, tiempo de finalizacion
+    global Q_limit
+    global num_events
+    global mean_interarrival
+    global mean_service
     global num_delays_required
-    plt.figure(1)
-    plt.plot(ListaMediaDemoras)
-    listaDemoraTotal=[]
-    for i in range(len(ListaMediaDemoras)):
-        listaDemoraTotal.append(total_of_delays/num_delays_required)
-    plt.plot(ListaMediaDemoras)
-    plt.plot(listaDemoraTotal)
-    plt.show()
-
-def GraficasPromClientesSistema(arreglo):
-    #global num_clientes_sistema
     global numero_clientes_cola
+    global num_custs_delayed
+    global next_event_type
+
+    Q_limit=100
+    num_events=2
+    #mean_interarrival = float( input("media del arribo: "))
+    #mean_service=float(input("media del servicio: "))
+    #num_delays_required = float(input("num delays requiered: "))
+
+
+
+    #Inicializa
+
+    inicializar()
+
+
+
+    #continua 
+    #while(num_custs_delayed<num_delays_required):
+    #arribos=0
+    #departos=0
+
+    while(num_custs_delayed < num_delays_required):
+        timing()
+        update_time_avg_state()
+
+        if(next_event_type==1):
+            arrive()
+            #arribos+=1
+
+            
+        elif(next_event_type==2):
+            depart()
+            #departos+=1
+    #report()
+
+
+
+
+
+
+def GraficaPromClientesSistemaMedia(arreglo):
+    #global num_clientes_sistema
+     
     
+
+    global numero_clientes_cola
+    global media_gral
+    global varianza_gral
+    global desvio_gral
+    global contador_gral
+        
+ 
     listaClientes = []
 
     acum_clientes_sistema = np.cumsum(arreglo)
@@ -323,50 +332,109 @@ def GraficasPromClientesSistema(arreglo):
     num_clientes_sistema_media = []
     for i in range(len(arreglo)):
         num_clientes_sistema_media.append(acum_clientes_sistema[i]/(i+1))
-        
+            
 
-    num_clientes_sistema_varianza=[]
-    num_clientes_sistema_desviacion=[]
-    num_clientes_sistema_fr=[]
-    contador=0
-    k=0
-
-    for i in range(len(arreglo)):
-        listaClientes.append(arreglo[i])
-        num_clientes_sistema_varianza.append(np.var(listaClientes))
-        num_clientes_sistema_desviacion.append(np.std(listaClientes))
-        if(int(listaClientes[i]) == int(numero_clientes_cola)):
-            contador+=1
-            num_clientes_sistema_fr.append(contador/(i+1))
-            k=i
-        else:
-            num_clientes_sistema_fr.append(contador/(k+1))
+    
 
     media_cli_sistema=[]
     for i in range(len(arreglo)):
         media_cli_sistema.append(np.mean(arreglo))
 
-    varianza_cli_sistema=[]
+    
+    media_gral+=np.mean(arreglo)
+
+    
+    plt.figure(1)
+    plt.plot(time_global, num_clientes_sistema_media)
+    if (contador_gral == 10):
+        media_cli_sistema = []
+        for i in range(len(arreglo)):
+            media_cli_sistema.append(media_gral/10)
+        plt.plot(time_global, media_cli_sistema,label="Media esperada de clientes en el sistema")
+        plt.legend()
+    
+
+
+def GraficaPromClientesSistemaVarianza(arreglo):
+
+    global numero_clientes_cola
+    global media_gral
+    global varianza_gral
+    global desvio_gral
+    global contador_gral
+
+    listaClientes = []
+
+
+    
+
+    num_clientes_sistema_varianza = []
+
+    for i in range(len(arreglo)):
+        listaClientes.append(arreglo[i])
+        num_clientes_sistema_varianza.append(np.var(listaClientes))
+
+
+
+    varianza_cli_sistema = []
     for i in range(len(arreglo)):
         varianza_cli_sistema.append(np.var(arreglo))
+
+
+    varianza_gral += np.var(arreglo)
+
+    plt.figure(2)
+
+    plt.plot(time_global, num_clientes_sistema_varianza)
+    if (contador_gral == 10):
+        varianza_cli_sistema = []
+        for i in range(len(arreglo)):
+            varianza_cli_sistema.append(varianza_gral/10)
+        plt.plot(time_global, varianza_cli_sistema,label="Varianza esperada de clientes ")
+        plt.legend()
+
+
+def GraficaPromClientesSistemaDesvio(arreglo):
+    global numero_clientes_cola
+    global media_gral
+    global varianza_gral
+    global desvio_gral
+    global contador_gral
+
+    listaClientes = []
+
+
+ 
+
+    num_clientes_sistema_desviacion = []
+
+    for i in range(len(arreglo)):
+        listaClientes.append(arreglo[i])
+        num_clientes_sistema_desviacion.append(np.std(listaClientes))
+
+ 
 
     desviacion_cli_sistema = []
     for i in range(len(arreglo)):
         desviacion_cli_sistema.append(np.std(arreglo))
-    
-    
-   
-    plt.plot(time_global, num_clientes_sistema_media)
-    plt.plot(time_global,media_cli_sistema)
-    plt.show()
 
-    plt.plot(time_global, num_clientes_sistema_varianza)
-    plt.plot(time_global,varianza_cli_sistema)
-    plt.show()
+    desvio_gral += np.std(arreglo)
 
+    plt.figure(3)
     plt.plot(time_global, num_clientes_sistema_desviacion)
-    plt.plot(time_global,desviacion_cli_sistema)
-    plt.show()
+
+    if (contador_gral == 10):
+        desviacion_cli_sistema = []
+        for i in range(len(arreglo)):
+            desviacion_cli_sistema.append(desvio_gral/10)
+
+        plt.figure(3)
+        plt.plot(time_global, desviacion_cli_sistema,label="Desviacion esperada de clientes")
+        plt.legend()
+
+
+
+    
 
 
 def GraficasTiempoClientesSistema(arreglo):
@@ -445,6 +513,7 @@ def menu():
     print("3 - Tiempo promedio en sistema")
     print("4 - Tiempo promedio en cola")
     print("5 - Utilización del servidor")
+    print("6 - Probabilidad de n cliente en cola")
     print("0 - Salir")
 
 
@@ -455,23 +524,53 @@ while True:
 
     opcionMenu = input("Ingrese su opcion:  ")
     print(" ")
-    
+
     if opcionMenu == "1":
-        GraficasPromClientesSistema(num_clientes_sistema)
+        contador_gral=0
+        media_gral = 0
+        varianza_gral=0
+        desvio_gral=0
+        for i in range(10):
+            contador_gral+=1
+            MainProgram()
+            GraficaPromClientesSistemaMedia(num_clientes_sistema)
+            plt.title("Media de clientes en el sistema")
+            plt.xlabel("Tiempo(segundos)")  # título del eje x
+            plt.ylabel("Media de clientes")
+
+            GraficaPromClientesSistemaVarianza(num_clientes_sistema)
+            plt.title("Varianza de los clientes en el sistema")
+            plt.xlabel("Tiempo(segundos)")  # título del eje x
+            plt.ylabel("Varianza de clientes")
+            
+            GraficaPromClientesSistemaDesvio(num_clientes_sistema)
+            plt.title("Desviacion de los clientes en el sistema")
+            plt.xlabel("Tiempo(segundos)")  # título del eje x
+            plt.ylabel("Desviacion de clientes")
+        plt.show()
+        #plt.show()
+        
         input("Pulsa una tecla para continuar")
     elif opcionMenu == "2":
-        GraficasPromClientesSistema(area_num_in_q_total)
+        GraficaPromClientesSistemaMedia(area_num_in_q_total)
         input("Pulsa una tecla para continuar")
     elif opcionMenu == "3":
-        GraficasTiempoClientesSistema(tiempo_2)
+        GraficaPromClientesSistemaMedia(tiempo_2)
         input("Pulsa una tecla para continuar")
     elif opcionMenu == "4":
-        GraficasTiempoClientesSistema(ListaDemoras)
+        GraficaPromClientesSistemaMedia(ListaDemoras)
         input("Pulsa una tecla para continuar")
     elif opcionMenu == "5":
-        GraficasPromClientesSistema(utilizacion_servidor_total)
+        GraficaPromClientesSistemaMedia(utilizacion_servidor_total)
         PastelUtilizacionServidor()
 
+        input("Pulsa una tecla para continuar")
+
+    elif opcionMenu == "6":
+        numero_clientes_cola = input("Cuantos clientes en cola habra?")
+
+        prob_n_clientes_cola = ((1-(1/mean_interarrival)/(1/mean_service)) *((1/mean_interarrival)/(1/mean_service))**int(numero_clientes_cola))
+        print("La probabilidad de que haya: " + str(numero_clientes_cola) +" clientes en cola es de:"+str(prob_n_clientes_cola))
         input("Pulsa una tecla para continuar")
     elif opcionMenu == "0":
         break
